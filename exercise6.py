1957560361d4ac3f1252af23c4806f7d9794fc6a
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-10-11 10:42:24 -0400

    Work on 2c

--- exercise6.py
+++ exercise6.py
@@ -9,13 +9,23 @@ wages.head(n=5)
 import pandas as pd 
 iris=pd.read_csv('iris.csv')
 iris.shape
-#2a
-iris.iloc[148:,3:]
-#2b
-from collections import Counter 
-Species=iris.iloc[:,4:]
 
-#2c
+#2a : print the last 2 rows in the last 2 columns to the Python terminal
+iris.iloc[148:,3:]
+
+#2b : get the number of observations for each species included in the data set
+count=0
+cat while read line
+do
+    if echo $line | grep $stringChoice; then
+        count=$[ count + 1 ]
+done
+echo "Finished processing file"
+
+
+#2c : get rows with Sepal.Width > 3.5
 iris.iloc[:,1:2]>3.5
 
-#2d
+#2d : write the data for the species setosa to a comma-delimited file names ‘setosa.csv’
+
+#2e : calculate the mean, minimum, and maximum of Petal.Length for observations from virginica
